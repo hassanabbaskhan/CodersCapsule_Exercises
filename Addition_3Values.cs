5f2fb82895c7ac2865d9336a547ee3bd7b5ba3cc
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2024-06-06 02:46:13 +0500

    C# BASIC ALGORITHM EXERCISES (codersCapsule.com)
    1. Write a program to get 3 values from user and print sum of its square value.

---
+++ Addition_3Values.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Addition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int num3;
+
+            Console.Write("Enter the num 1  = ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the num 2  = ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the num 3  = ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+
+            int sum = num1 + num2 + num3;
+
+            //text formatting is being used here easy to understand.
+
+            Console.WriteLine("Sum of {0},{1} and {2} equals {3}", num1, num2, num3,sum);
+            Console.ReadKey();
+
+        }
+    }
+}
