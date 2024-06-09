d20e77d8b908f6427a52ac879adf0e496cc4ee0e
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2024-06-09 10:09:22 +0500

    Addition of Cube of input values
    This exercise uses basic math funtion and educate about it's use.
    remember Math.Pow method works with double datatype only

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Addition_of_valueCubes
+{
+
+    //2. Write a program to get 3 values from user and print sum of its cube value.
+
+
+    class addition
+    {
+        static void Main()
+        {
+            double num1, num2, num3, sum;
+
+            Console.Write("Enter num1 = ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter num2 = ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter num3 = ");
+            num3 = Convert.ToDouble(Console.ReadLine());
+
+            sum = Math.Pow(num1, 3) + Math.Pow(num2, 3) + Math.Pow(num3, 3); //Math.Pow function executes only if datatype is double
+
+            Console.Write("The sum of cubes of {0}, {1}, {2} is {3}", num1, num2, num3, sum);
+            Console.ReadKey();
+
+        }
+    }
+}
