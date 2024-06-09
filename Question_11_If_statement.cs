a8164cb67934caa60e5c64f1e730b8125c58fe63
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2024-06-09 18:30:46 +0500

    Question_11

---
+++ Question_11_If_statement.cs
@@ -0,0 +1,35 @@
+﻿//11.Write a program to get two integers and check whether they are equal or not.
+
+using System;
+
+namespace Question_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.Write("Enter num1 = ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter num2 = ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //checking conditions
+
+            if (num1 != num2)
+            {
+                Console.WriteLine("Both entered integers are not same");
+            }
+            else
+            {
+                Console.WriteLine("Both the entered integers are same");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
