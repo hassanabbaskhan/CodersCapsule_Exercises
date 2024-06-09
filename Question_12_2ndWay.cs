846c9ffd473e61a12f79bdd8f4e76cd4719f95f0
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2024-06-10 00:32:41 +0500

    Question_12_2ndWay_Jump Statement

---
+++ Question_12_2ndWay.cs
@@ -0,0 +1,41 @@
+﻿//12. Write a program to check whether a given number is positive or negative.
+//Here we will use jump statement to call the program continuously.
+
+
+using System;
+
+namespace Question_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+
+            int num1;
+
+            Console.Write("Enter a number = ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 < 0)
+            {
+                Console.WriteLine("{0} is a negative number", num1);
+            }
+
+            else if (num1 > 0)
+            {
+                Console.WriteLine("{0} is a positive number", num1);
+
+            }
+
+            else
+            {
+                Console.WriteLine("{0} is a whole number", num1);
+            }
+            
+
+            goto    start;
+
+        }
+    }
+}
