a5d37d0f32f5782ffb913d01d6882bd3261b27eb
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2024-06-24 12:27:40 -0700

    Question_22_DoWhileLoop.cs
    Question_22_DoWhileLoop.cs

---
+++ Question_22_DoWhileLoop.cs.cs
@@ -0,0 +1,30 @@
+﻿
+/*
+22. Write a program to display the first 10 natural numbers using do while loop.
+
+ 
+ */
+
+using System;
+
+namespace Question_22_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+
+            do
+            {
+                Console.WriteLine(x);
+                x = x + 2;
+            } 
+            
+            while (x <= 20);
+
+            Console.ReadLine();
+
+        }
+    }
+}
