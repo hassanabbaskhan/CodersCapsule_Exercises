c68be9dfab24cfe0426e8e1dd9fcb38e0c2ecd51
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2024-06-24 12:36:54 -0700

    Question_22_ForLoop.cs
    Question_22_ForLoop.cs

---
+++ Question_22_ForLoop.cs.cs
@@ -0,0 +1,26 @@
+﻿
+/*
+22. Write a program to display the first 10 natural numbers using for loops.
+ 
+ */
+
+using System;
+
+namespace Question_22_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            for ( x = 2; x <= 20; x = x + 2)
+
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
