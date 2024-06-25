024e33e056977782929b82962401d1e573a7119e
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2024-06-25 12:08:29 -0700

    Question_22_For_Loop
    Question_22_For_Loop

---
+++ Question_22_For_Loop.cs
@@ -0,0 +1,27 @@
+﻿/*
+ 22. Write a program to display the first 10 natural numbers using while loops.
+ 
+ */
+
+using System;
+
+
+namespace Question_22_While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+
+            while (x <= 10 )
+            {
+                Console.WriteLine(x);
+                x++;
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
